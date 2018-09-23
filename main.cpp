fb12c961164d154ba79a94f6f0f9da49476aaa87
Connor Hewitt <connor.hewitt@gmail.com>
2018-09-23 01:40:01 -0600

    Actually use traits!

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
+#include <iostream>
+
 #include "catdog.hpp"
+#include "cat.hpp"
+#include "dog.hpp"
 
 int main(int argc, char** argv) {
     Catdog catdog;
     catdog.speak();
+
+    std::cout << "With traits:" << std::endl;
+    catdog.as<Dog>()->bark();
+    catdog.as<Cat>()->meow();
 }
